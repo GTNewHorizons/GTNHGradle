4134159c0f660e2601c888dcf467e83c0aa8f99e
eigenraven <eigenraven@users.noreply.github.com>
2023-05-02 10:14:34 +0000

    Deploy javadoc from ref master

---
+++ master/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.gtnewhorizons.gtnhgradle","l":"GTNHGradlePlugin.GTNHExtension"},{"p":"com.gtnewhorizons.gtnhgradle","l":"GTNHGradlePlugin"}];updateSearchResults();
