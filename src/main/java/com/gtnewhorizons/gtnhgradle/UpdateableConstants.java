903e1e594f69c513dc9a1d0650b76eb6ca6db095
Raven Szewczyk <git@eigenraven.me>
2024-03-12 13:28:07 +0000

    Lwjgl3ify 2.0 run configuration support (#11)
    * Minor fixes to disabling code style module/modern java module
    
    * Update to lwjgl3ify 2.0.0
    
    * Fix warnings in UpdateDependenciesTask

--- src/main/java/com/gtnewhorizons/gtnhgradle/UpdateableConstants.java
+++ src/main/java/com/gtnewhorizons/gtnhgradle/UpdateableConstants.java
@@ -26,10 +26,10 @@ public class UpdateableConstants {
     public static final @NotNull String NEWEST_GTNH_LIB = "com.github.GTNewHorizons:GTNHLib:0.2.10";
     /** Latest version of GTNHLib for modern Java support */
     // https://github.com/GTNewHorizons/lwjgl3ify/releases
-    public static final @NotNull String NEWEST_LWJGL3IFY = "com.github.GTNewHorizons:lwjgl3ify:1.5.16";
+    public static final @NotNull String NEWEST_LWJGL3IFY = "com.github.GTNewHorizons:lwjgl3ify:2.0.0";
     /** Latest version of GTNHLib for modern Java support */
     // https://github.com/GTNewHorizons/Hodgepodge/releases
-    public static final @NotNull String NEWEST_HODGEPODGE = "com.github.GTNewHorizons:Hodgepodge:2.4.34";
+    public static final @NotNull String NEWEST_HODGEPODGE = "com.github.GTNewHorizons:Hodgepodge:2.4.35";
     /** Latest version of LWJGL3 for modern Java support */
     // https://github.com/LWJGL/lwjgl3/releases - but check what latest Minecraft uses too
     public static final @NotNull String NEWEST_LWJGL3 = "3.3.2";
