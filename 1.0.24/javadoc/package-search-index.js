752c362e48ea21d49a55ee804418522fced480d5
mitchej123 <mitchej123@users.noreply.github.com>
2024-07-23 17:59:22 +0000

    Deploy javadoc from ref 1.0.24

---
+++ 1.0.24/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.gtnewhorizons.gtnhgradle"},{"l":"com.gtnewhorizons.gtnhgradle.modules"},{"l":"com.gtnewhorizons.gtnhgradle.tasks"}];updateSearchResults();
